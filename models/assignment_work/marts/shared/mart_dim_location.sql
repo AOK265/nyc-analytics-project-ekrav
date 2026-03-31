db37e32adb2c62c3ec70b254fddb9ad855f50957
Evan Kravitz <evankrav@gmail.com>
2026-03-31 02:52:08 +0000

    made shared 2

--- models/assignment_work/marts/shared/mart_dim_location.sql
+++ models/assignment_work/marts/shared/mart_dim_location.sql
@@ -4,7 +4,7 @@ WITH all_locations AS (
    -- Get locations from 311 requests
    SELECT DISTINCT
       borough,
-      incident_zip --TODO replace (HINT: look @ dimensional model & staging data!)
+      incident_zip as zip_code --TODO replace (HINT: look @ dimensional model & staging data!)
    FROM {{ ref('stg_nyc_311_dot') }}
    WHERE borough IS NOT NULL
 
@@ -13,7 +13,7 @@ WITH all_locations AS (
    -- Get locations from restaurant applications
    SELECT DISTINCT
        borough,
-       zip -- TODO replace (HINT: look @ dimensional model & staging data!)
+       zip as zip_code -- TODO replace (HINT: look @ dimensional model & staging data!)
    FROM {{ ref('stg_nyc_restaurant_app') }}
    WHERE borough IS NOT NULL
 ),
