dcb730546d410d85cec8d5e7e4829c01a7261114
Evan Kravitz <evankrav@gmail.com>
2026-03-31 03:01:37 +0000

    again lol

--- models/assignment_work/marts/open_restaurant_apps/mart_dim_seating_type.sql
+++ models/assignment_work/marts/open_restaurant_apps/mart_dim_seating_type.sql
@@ -5,7 +5,7 @@ WITH seating_types AS (
        case
             when approved_for_sidewalk_seating = 'yes' then TRUE else FALSE end as approved_for_sidewalk,
         case    
-            when approved_for_roadway_seating 'yes' then TRUE else FALSE end
+            when approved_for_roadway_seating = 'yes' then TRUE else FALSE end
 --TODO: Replace this comment with a CASE WHEN .. statement that handles the different possibilities for approved_for_sidewalk_seating and approved_for_roadway_seating in the data
 --NOTE: The final result we want to select here is two boolean columns (TRUE or FALSE values in them), one column approved_for_sidewalk (TRUE or FALSE value), and one column approved_for_roadway 
       AS approved_for_roadway
