5a37d78443068f329b8c9d903e348c1cb31e2bbe
Evan Kravitz <evankrav@gmail.com>
2026-04-25 20:17:45 +0000

    dim locat

---
+++ models/project_work/marts/shared/dim_location.sql
@@ -0,0 +1,29 @@
+-- Location dimension shared by both vehicle collisions and 311 service reqs
+
+WITH all_locations AS (
+   -- Get locations from 311 requests
+   SELECT DISTINCT
+      borough,
+      incident_zip as zip_code 
+   FROM {{ ref('stg_nyc_311_street_condition') }}
+   WHERE borough IS NOT NULL
+
+   UNION DISTINCT
+
+   -- Get locations from vehicle collisions
+   SELECT DISTINCT
+       borough,
+       zip_code 
+   FROM {{ ref('staging_tbl_veh_collision') }}
+   WHERE borough IS NOT NULL
+),
+
+location_dimension AS (
+   SELECT
+       {{ dbt_utils.generate_surrogate_key(['borough', 'zip_code']) }} AS location_key,
+       borough,
+       zip_code
+   FROM all_locations
+)
+
+SELECT * FROM location_dimension
